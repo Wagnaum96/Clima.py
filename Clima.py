08c92034b4a2c0ed5091bf08237e6a1c5e487d9b
Wagnaum96 <102704633+Wagnaum96@users.noreply.github.com>
2022-04-21 11:31:46 -0300

    Update Clima.py

--- Clima.py
+++ Clima.py
@@ -1,40 +1,45 @@
-from distutils.cmd import Command
 import requests
 import json
-from tkinter import *  
+from tkinter import *
 
 
 
-api_key = "cf924283a722cd9965398c18456f18c7"
- 
+api_key = " - cole sua chave aqui - "
 
-def temperatura(): 
+
+def temperatura():
     cidade = entrada.get()
     link = f"https://api.openweathermap.org/data/2.5/weather?q={cidade}&appid={api_key}&lang=pt_br"
     requisicao = requests.get (link)
     requisicao.dic = requisicao.json()
     descricao = requisicao.dic['weather'][0]['description']
     temperatura1 = requisicao.dic['main']['temp'] - 273.15
-    
-    tempo['text'] = (descricao, f"{temperatura1:.2f}C")
+
+    tempo['text'] = (descricao, f"{temperatura1:.2f}°C")
 
 janela = Tk()
 janela.title("CLIMA")
-janela.geometry("300x300")
-
-texto = Label(janela, text="Clique no botão para saber a temperatura de hoje")
+janela.geometry("510x300")
+janela.configure(bg="#2F4F4F")
+texto = Label(janela, text="Clique no botão para saber o clima de hoje",
+              font="Verdana 15 bold",
+              bg="#2F4F4F", fg="#B0C4DE")
 texto.grid(column=0, row=0, padx=12, pady=12)
 
-city = Label(janela, text="Diga sua cidade")
+city = Label(janela, text="Diga sua cidade", font="Verdana 10 bold",
+             bg="#2F4F4F", fg="#B0C4DE")
 city.grid(column=0, row=2)
 
 entrada= Entry(janela, width=10)
 entrada.grid(column=0, row=3)
 
-botao = Button(janela, text="Tempo hoje", command=temperatura)
-botao.grid(column=0, row= 5, padx=12, pady=12)
+botao = Button(janela, text="Buscar",
+               font="Verdana 15 bold",
+               command=temperatura,
+               bg="#2F4F4F", fg="#B0C4DE")
+botao.grid(column=0, row= 4, padx=12, pady=12)
 
-tempo = Label(janela, text='')
+tempo = Label(janela, text='',bg="#2F4F4F", fg="#B0C4DE", font="Verdana 10 bold")
 tempo.grid(column=0, row=6)
 
 
